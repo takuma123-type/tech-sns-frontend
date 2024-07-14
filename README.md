6fc420f64b63ddd51084879e6f7884f747708515
Takuma <>
2024-07-14 12:13:08 +0900

    tailwindとflowbiteの導入

---
+++ README.md
@@ -0,0 +1,5 @@
+# reference
+ ## React TypeScript
+ - https://qiita.com/91works-i-kato/items/9f9ad03fee32d42ed547
+ ## Tailwind
+ - https://qiita.com/y_kato_eng/items/439dae65ba13d050c91e
---
+++ app/src/components/pages/Index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../molecules/Header";
+
+export default function Index() {
+  return (
+    <React.Fragment>
+      <>
+      <Header />
+      </>
+    </React.Fragment>
+  );
+}
